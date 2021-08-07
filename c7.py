839d699ab9551c6ddc1b9983c7c2cbca8894fe97
fitz1987 <23111522+fitz1987@users.noreply.github.com>
2021-08-06 20:11:28 -0500

    Update c7.py

--- c7.py
+++ c7.py
@@ -2,6 +2,7 @@
 # https://en.wikipedia.org/wiki/Advanced_Encryption_Standard 
 # http://blog.joshuahaddad.com/cryptopals-challenges-7-8/
 # https://techtutorialsx.com/2018/04/09/python-pycrypto-using-aes-128-in-ecb-mode/
+# https://laconicwolf.com/2018/07/22/cryptopals-challenge-7-implement-aes-in-ecb-mode-with-python/
 # https://www.adamberent.com/wp-content/uploads/2019/02/AESbyExample.pdf 
 
 # my code 
