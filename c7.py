25388272b6c2407967bf34ad1168c5dbfd682783
fitz1987 <23111522+fitz1987@users.noreply.github.com>
2021-08-06 19:44:57 -0500

    Create c7.py
    initial commit with references; no code here yet

---
+++ c7.py
@@ -0,0 +1,7 @@
+# References for challenge 7: 
+# https://en.wikipedia.org/wiki/Advanced_Encryption_Standard 
+# http://blog.joshuahaddad.com/cryptopals-challenges-7-8/
+# https://techtutorialsx.com/2018/04/09/python-pycrypto-using-aes-128-in-ecb-mode/
+# https://www.adamberent.com/wp-content/uploads/2019/02/AESbyExample.pdf 
+
+# my code 
