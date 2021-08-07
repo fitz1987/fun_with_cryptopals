f1bb22cc8d3f211438553baf21554500b1ead099
fitz1987 <23111522+fitz1987@users.noreply.github.com>
2021-08-06 20:08:06 -0500

    Update c7.py

--- c7.py
+++ c7.py
@@ -5,3 +5,47 @@
 # https://www.adamberent.com/wp-content/uploads/2019/02/AESbyExample.pdf 
 
 # my code 
+# c7.py
+# text is in c7file
+# located in the same directory as this script file
+"""
+A text file has been encrypted using AES-128 in ECB mode
+via the key "YELLOW SUBMARINE"
+(all caps, space matters, ignore quote marks)
+
+decrypt it
+- write your own code, don't use a command line tool
+- apparently this will be useful later on in cryptopals
+"""
+import base64
+from Crypto.Cipher import AES
+
+# get the text into a variable
+# and convert to bytes
+with open('c7file') as inp:
+    text = base64.b64decode(inp.read())
+print(text) # check that it worked: it did
+
+def aes_ecb_encrypt(plaintext, key):
+    cipher = AES.new(key, AES.MODE_ECB)
+    return(cipher.encrypt(plaintext))
+
+def aes_ecb_decrypt(key, ciphertext):
+    cipher = AES.new(key, AES.MODE_ECB)
+    return(cipher.decrypt(ciphertext))
+
+cle="YELLOW SUBMARINE"
+print(aes_ecb_decrypt(cle, text))
+
+new = aes_ecb_decrypt(cle, text)
+
+# turn new thing back to hex string from bytes
+new_hex = new.hex()
+
+# write solution to file
+with open("newtext.txt", "w") as output:
+    output.write(new_hex)
+
+# the encrypt it again :)
+new_byt = base64.b64decode(new_hex)
+print(aes_ecb_encrypt(new_byt, cle))
